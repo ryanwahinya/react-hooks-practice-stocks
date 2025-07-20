eeaf4bd92f7cb28dd2e2f031973a77594594fa97
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-07-20 22:19:09 +0300

    final

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [sortType, setSortType] = useState("");
+  const [filterType, setFilterType] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((res) => res.json())
+      .then(setStocks);
+  }, []);
+
+  function handleBuy(stock) {
+    if (!portfolio.includes(stock)) {
+      setPortfolio([...portfolio, stock]);
+    }
+  }
+
+  function handleSell(stock) {
+    setPortfolio(portfolio.filter((s) => s.id !== stock.id));
+  }
+
+  function handleSort(type) {
+    setSortType(type);
+  }
+
+  function handleFilter(type) {
+    setFilterType(type);
+  }
+
+  const filteredStocks = stocks.filter((stock) =>
+    filterType ? stock.type === filterType : true
+  );
+
+  const sortedStocks = [...filteredStocks].sort((a, b) => {
+    if (sortType === "Alphabetically") {
+      return a.name.localeCompare(b.name);
+    } else if (sortType === "Price") {
+      return a.price - b.price;
+    }
+    return 0;
+  });
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSortChange={handleSort} onFilterChange={handleFilter} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={sortedStocks} onStockClick={handleBuy} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} onStockClick={handleSell} />
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks, onStockClick }) {
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock) => (
+        <Stock
+          key={stock.id}
+          stock={stock}
+          onClick={() => onStockClick(stock)}
+        />
+      ))}
     </div>
   );
 }
+
 
 export default StockContainer;
